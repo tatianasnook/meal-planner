3b550107d977482e1b91bfd5b336949de7749299
tatianasnook <tatsnook@gmail.com>
2024-03-06 19:34:08 -0600

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.set('strictQuery', false);
 
-const PORT = 4000;
+const PORT = 4000 || process.env.port
+
+mongoose
+.connect(process.env.MONGOBD_LINK)
+.then(() => console.log('WE WERE CONNECTED TO MONGO'))
+.catch((err) => console.log(err))
 
 app.listen(PORT, () => {
   console.log(`I AM LISTENING on PORT ${PORT}`)
