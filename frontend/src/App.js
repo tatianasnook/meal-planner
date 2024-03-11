8613109397882938821143f5f967a0cdd3ae65e8
tatianasnook <tatsnook@gmail.com>
2024-03-11 17:10:48 -0500

    added frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Menu Plan</h1>
+      <input
+      type="text"
+      placeholder="Add a meal"
+      />
+      <button>ADD</button>
+    </div>
+  )
+}
+
+export default App;
