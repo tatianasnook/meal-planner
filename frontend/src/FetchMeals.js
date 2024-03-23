80fa92347bb4e15f85342ffe4c990eb4e0801cff
tatianasnook <tatsnook@gmail.com>
2024-03-23 15:16:39 -0500

    connected to mongodb

--- frontend/src/FetchMeals.js
+++ frontend/src/FetchMeals.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const getAllMeals = (setMeal) => {
-  axios.get("http://localhost:4000")
+  axios.get("https://meal-plan-n3lk.onrender.com")
   .then(({data}) => {console.log(data)
   setMeal(data)
   })
 }
 
 const addMeal = (title, setTitle, setMeal) => {
-  axios.post("http://localhost:4000/saveMeals", { title })
+  axios.post("https://meal-plan-n3lk.onrender.com/saveMeals", { title })
   .then((data) => {
     console.log(data)
     setTitle("")
@@ -17,7 +17,7 @@ const addMeal = (title, setTitle, setMeal) => {
 }
 
 const editMeal = (mealId, title, setTitle, setMeal, setEditing) =>  {
-  axios.post("http://localhost:4000/editMeal", { _id: mealId, title })
+  axios.post("https://meal-plan-n3lk.onrender.com/editMeal", { _id: mealId, title })
   .then((data) => {
     console.log(data)
     setTitle("")
@@ -27,7 +27,7 @@ const editMeal = (mealId, title, setTitle, setMeal, setEditing) =>  {
 }
 
 const deleteMeal = (_id, setMeal) => {
-  axios.post("http://localhost:4000/deleteMeal", { _id })
+  axios.post("https://meal-plan-n3lk.onrender.com/deleteMeal", { _id })
   .then((data) => {
     console.log(data)
     getAllMeals(setMeal)
